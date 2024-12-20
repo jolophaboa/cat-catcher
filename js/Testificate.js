f350ab655699b88c7f2e14bf1c477e918644b9c5
jolophaboa <120971331+jolophaboa@users.noreply.github.com>
2024-12-20 12:44:56 -0500

    4 new cat skins
    4 new cat skins

---
+++ js/Testificate.js
@@ -0,0 +1,4 @@
+let names = ["jason", "laura", "victor", "rachael", "sarah", "damian", "alfred", "jens", "Jason", "Laura", "Victor", "Rachael", "Sarah", "Damian", "Alfred", "Jens"]
+for (name of names){
+    console.log("their name is:", name)
+}
