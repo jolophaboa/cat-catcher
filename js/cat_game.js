19f41d181acd2e77e93675e501de73f152d94a71
jolophaboa <120971331+jolophaboa@users.noreply.github.com>
2025-01-08 11:17:25 -0500

    Added a grey and orange cat

--- js/cat_game.js
+++ js/cat_game.js
@@ -260,14 +260,17 @@ const possibleCats = [
     ['cat_in_suit.png','Hungry_cat.png'],
     ['calico_cat.png','Hungry_cat.png'],
     ['black_cat.png','Hungry_cat.png'],
-    ['rapper_cat.png', 'Hungry_cat.png'],
-    ['police_cat.png','Hungry_cat.png'],
+    //['rapper_cat.png', 'Hungry_cat.png'],
+    //['police_cat.png','Hungry_cat.png'],
     ['white_cat.png','Hungry_cat.png'],
-    ['prisoner_cat.png','Hungry_cat.png'],
-    ['evil_cat.png', 'Hungry_cat.png'],
+   // ['prisoner_cat.png','Hungry_cat.png'],
+    //['evil_cat.png', 'Hungry_cat.png'],
     ['unicorn_cat.png','Hungry_cat.png'],
-    ['swamp_cat.png','Hungry_cat.png'],
+    //['swamp_cat.png','Hungry_cat.png'],
     ['tiger_cat.png','Hungry_cat.png'],
+    //['scarecrow_cat.png','Hungry_cat.png'],
+    ['grey_cat.png','Hungry_cat.png'],
+    ['orange_cat.png','Hungry_cat.png'],
 ];
 
 function randomCat() {
