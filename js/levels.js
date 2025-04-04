fc349dd62ac3f5c0b9f6aa8e341c367c06175350
Abe Fettig <abe@fettig.net>
2025-04-04 10:18:52 -0400

    Add placeholder file for defining levels

---
+++ js/levels.js
@@ -0,0 +1,16 @@
+const level1 = `
+XXXXXXXXXXXXXXXXXXXXXXXX
+X                      X
+X              ######  X
+X                      X
+                       X
+X            00        X
+X           000        X
+X            00        X
+X                      X
+X                      X
+X                      X
+X                      X
+X                      X
+XXXXXXXXXXXXXXXXXXXXXXXX
+`;
